9c63e5b5521713ed9412ab5c2d96a13b8d00e48e
Raphael Vicente Rosa <raphaelvrosa@gmail.com>
2018-11-27 22:38:07 -0200

    Adds updated Suricata Dockerfile and config files

--- docker-vnfs/suricata-ids/suricata/start.sh
+++ docker-vnfs/suricata-ids/suricata/start.sh
@@ -5,6 +5,9 @@ date > /usr/local/var/log/suricata/start.txt
 ip addr flush dev $IFIN
 ip addr flush dev $IFOUT
 
+ifconfig $IFIN promisc
+ifconfig $IFOUT promisc
+
 # bridge interfaces (layer 2) and let snort listen to bridge (IDS mode)
 brctl addbr br0
 brctl addif br0 $IFIN $IFOUT
@@ -15,12 +18,17 @@ ethtool -K br0 gro off
 rm -f /usr/local/var/run/suricata.pid
 rm -rf /usr/local/var/log/suricata/*
 
-suricata -c /usr/local/etc/suricata/suricata-ids.yaml --af-packet -D
+# suricata -c /usr/local/etc/suricata/suricata-ids.yaml --af-packet -D
 
-echo "Suricata VNF started ..."
-
-# if [ -z $1 ]; then
-# 	suricata -c /usr/local/etc/suricata/suricata-ids.yaml --af-packet
-# elif [ $1 == "b" ]; then
-# 	suricata -c /usr/local/etc/suricata/suricata-ids.yaml --af-packet -D
-# fi
+if [ -z $1 ]; then
+	suricata -c /usr/local/etc/suricata/suricata-ids.yaml --af-packet -D
+    echo "Suricata VNF started - AVG ruleset ..."
+elif [ $1 == "small_ruleset" ]; then
+	suricata -c /usr/local/etc/suricata/suricata-ids-small.yaml --af-packet -D
+    echo "Suricata VNF started - SMALL ruleset ..."
+elif [ $1 == "big_ruleset" ]; then
+	suricata -c /usr/local/etc/suricata/suricata-ids-big.yaml --af-packet -D
+    echo "Suricata VNF started - BIG ruleset ..."
+elif [ $1 == "empty" ]; then
+    echo "Suricata VNF NOT started ..."
+fi
